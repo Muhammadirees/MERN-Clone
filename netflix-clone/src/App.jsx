15b4d201a644e3aae796753406261b0b354a9f1d
Muhammad Idrees <muhammadidress2000@gmail.com>
2024-04-29 15:40:37 +0500

    Initail commit

---
+++ netflix-clone/src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div className="">Netflex clone </div>;
+};
+
+export default App;
---
+++ netflix-clone/src/pages/Player/Player.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./PLayer.css";
+
+const PLayer = () => {
+  return <div></div>;
+};
+
+export default PLayer;
